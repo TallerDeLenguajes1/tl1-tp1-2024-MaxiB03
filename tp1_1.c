94267b16d7388ad5dc10886ba694f40a2e9178f5
batallan <batallanmaximo03@gmail.com>
2024-04-16 12:25:35 -0300

    segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main() {
-    printf("hola mundo");
+int main() 
+{
+    printf("hola mundo\n");
+    int aux=10;
+    int *puntero;
+
+    puntero=&aux;
+
+    //Mostrar Contenido del puntero
+    printf("Contenido del puntero: %d\n", *puntero);
+
+    //Mostrar la Direccion de memoria almacenada por el puntero
+    printf("Direccion de memoria almacenada por el puntero: %p\n", puntero);
+
+    //Mostrar Direccion de memoria de la variable
+    printf("Direccion de memoria de la variable: %p\n",&aux);
+
+    //Mostrar Direccion de memoria del puntero
+    printf("Direccion de memoria del puntero: %p\n",&puntero);
+
+    //Mostrar el tamanio de la memoria utilizado por la variable
+    printf("tamanio de la variable: %d", sizeof(aux));
+
+    return 0;
 }
